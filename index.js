1b1c518f0387fc85d240d23665236a1e6142bdfc
ozhni <mahsaqholami@gmail.com>
2024-02-06 16:30:23 +0330

    make days work

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 "use strict";
+
 const monthEl = document.querySelector(".date h1");
 const fullDateEl = document.querySelector(".date p");
+const daysEl = document.querySelector(".days");
+
 const monthInx = new Date().getMonth();
+const lastDay = new Date(new Date().getFullYear(), monthInx + 1, 0).getDate();
+const firstDay = new Date(new Date().getFullYear(), monthInx, 1).getDay() - 1;
+console.log(firstDay);
 
 const months = [
   "January",
@@ -20,3 +26,17 @@ const months = [
 
 monthEl.innerText = months[monthInx];
 fullDateEl.innerHTML = new Date().toDateString();
+
+let days = "";
+
+for (let i = firstDay; i > 0; i--) {
+  days += `<div class="empty"></div>`;
+}
+for (let i = 1; i <= lastDay; i++) {
+  if (i === new Date().getDate()) {
+    days += `<div class="today">${i}</div>`;
+  } else {
+    days += `<div>${i}</div>`;
+  }
+}
+daysEl.innerHTML = days;
